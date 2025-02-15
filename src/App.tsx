19c3a5cb16dd8dc166acc5cc59be5a4fbf0dbc2e
lemjimeno <lemjimeno0120@gmail.com>
2025-02-15 14:34:07 -0500

    header footer and landing init

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Landing from "./app/pages/Landing";
+import Header from "./app/components/Header";
+import Footer from "./app/components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="app">
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
