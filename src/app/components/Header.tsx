a0b72940d504906b4fc1e2a9af2b350967264409
Elliot <97140324+elliot-jimeno@users.noreply.github.com>
2025-02-16 17:52:47 -0500

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,29 @@
 import "./Header.css";
 
 function Header() {
-  return <div id="header">Header with transparent background</div>;
+  return (
+    <div id="header">
+      <div id="header-container">
+        <div id="header-logo"></div>
+        <a className="header-nav" href="">
+          Features
+        </a>
+        <a className="header-nav" href="">
+          Solutions
+        </a>
+        <a className="header-nav" href="">
+          Resources
+        </a>
+        <a className="header-nav" href="">
+          Pricing
+        </a>
+        <a className="header-login" href="">
+          Login
+        </a>
+        <div className="header-signup">Start Your Free Trial</div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
